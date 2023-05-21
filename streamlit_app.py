3ca58df547e1cdd8571e1132eb73e2f90f9bcaa6
snowflakepallavi <133100021+snowflakepallavi@users.noreply.github.com>
2023-05-21 11:16:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-add_fruit = streamlit.text_input('What fruit would you like to add into list?','Jackfruit')
-streamlit.write('Thanks for adding ', add_fruit)
-
-
-import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values (add_fruit)")
-
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -59,4 +49,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_fruit = streamlit.text_input('What fruit would you like to add into list?','Jackfruit')
+streamlit.write('Thanks for adding ', add_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
